0ee4b153da8fe4ec2112547e90b48ffece4d469d
jhonatan torres <jtorresf@unal.edu.co>
2020-07-06 13:21:56 -0500

    first commit

---
+++ precache-manifest.ca10071c5034271436a1df88c0413afd.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4de7053e004c2334dbb838958158a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9771ccd61004e1fd6fb3",
+    "url": "/static/css/main.e932a121.chunk.css"
+  },
+  {
+    "revision": "852cfe37cf7ddc2409bc",
+    "url": "/static/js/2.2f74a160.chunk.js"
+  },
+  {
+    "revision": "b619ef5300e807bfdefd6f1e7408f364",
+    "url": "/static/js/2.2f74a160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9771ccd61004e1fd6fb3",
+    "url": "/static/js/main.889b9b30.chunk.js"
+  },
+  {
+    "revision": "2ea8476b57ff0e11f952",
+    "url": "/static/js/runtime-main.359f4dec.js"
+  },
+  {
+    "revision": "3656110a0eba77d619863b0e34cdc897",
+    "url": "/static/media/Biotif-Bold.3656110a.ttf"
+  },
+  {
+    "revision": "5aab6d43edfac2a05fbba5297888057c",
+    "url": "/static/media/Biotif-Bold.5aab6d43.svg"
+  },
+  {
+    "revision": "60c571afa17d4452a32bb91f22e439e5",
+    "url": "/static/media/Biotif-Bold.60c571af.woff2"
+  },
+  {
+    "revision": "6f3ed5413c4dd54ba72c3476cfbb1c01",
+    "url": "/static/media/Biotif-Bold.6f3ed541.eot"
+  },
+  {
+    "revision": "758733f873a339d6a01d38d123dd9c3c",
+    "url": "/static/media/Biotif-Bold.758733f8.woff"
+  },
+  {
+    "revision": "1f6b5dde6ce1bd3654d2306c70a8abbd",
+    "url": "/static/media/Biotif.1f6b5dde.svg"
+  },
+  {
+    "revision": "310ac89bbe5b463d8efd7033e63151a5",
+    "url": "/static/media/Biotif.310ac89b.woff2"
+  },
+  {
+    "revision": "8386358d0bb257eb0a0de2004cac132e",
+    "url": "/static/media/Biotif.8386358d.ttf"
+  },
+  {
+    "revision": "cfeec6d8b5615cc29d5fa88d485a3a39",
+    "url": "/static/media/Biotif.cfeec6d8.eot"
+  },
+  {
+    "revision": "df97821b00c8d0cd79f3ff3cab52e0ba",
+    "url": "/static/media/Biotif.df97821b.woff"
+  },
+  {
+    "revision": "04dcfb2adf199ea1e5e6289623b0acbc",
+    "url": "/static/media/Gilroy-ExtraBold.04dcfb2a.svg"
+  },
+  {
+    "revision": "699deb68d2eba35ac8c0e345047b5637",
+    "url": "/static/media/Gilroy-ExtraBold.699deb68.ttf"
+  },
+  {
+    "revision": "9113372ceacb4b794b83d39362d2055a",
+    "url": "/static/media/Gilroy-ExtraBold.9113372c.woff"
+  },
+  {
+    "revision": "e903dd6537f8d86d025988eade5324d3",
+    "url": "/static/media/Gilroy-ExtraBold.e903dd65.woff2"
+  },
+  {
+    "revision": "ef47697496e5d33a45a9b009438f36a6",
+    "url": "/static/media/Gilroy-ExtraBold.ef476974.eot"
+  },
+  {
+    "revision": "017e2eff217789c477d7081fc16ee5ed",
+    "url": "/static/media/Gilroy-Light.017e2eff.eot"
+  },
+  {
+    "revision": "64488e9410cd0a9cda06c8582c0d182b",
+    "url": "/static/media/Gilroy-Light.64488e94.woff2"
+  },
+  {
+    "revision": "752c1d40bab9991b008ca86a253b4ecf",
+    "url": "/static/media/Gilroy-Light.752c1d40.woff"
+  },
+  {
+    "revision": "a52b3d442d2045570284b8bb853f1f14",
+    "url": "/static/media/Gilroy-Light.a52b3d44.ttf"
+  },
+  {
+    "revision": "ae8447cfa7ab8ab4a4293735448ac9e9",
+    "url": "/static/media/Gilroy-Light.ae8447cf.svg"
+  },
+  {
+    "revision": "20b3af5ec843186407225242639cf9a5",
+    "url": "/static/media/Montserrat-Black.20b3af5e.woff2"
+  },
+  {
+    "revision": "304296c4425c489d386cd9c5d567836d",
+    "url": "/static/media/Montserrat-Black.304296c4.svg"
+  },
+  {
+    "revision": "b539ef9649cf108b5930bee935dc752f",
+    "url": "/static/media/Montserrat-Black.b539ef96.eot"
+  },
+  {
+    "revision": "c9226b72090651d5d4ff6abaf526489b",
+    "url": "/static/media/Montserrat-Black.c9226b72.ttf"
+  },
+  {
+    "revision": "f7e4099337bc677c148c0f035c34f5d6",
+    "url": "/static/media/Montserrat-Black.f7e40993.woff"
+  },
+  {
+    "revision": "425e4899112f0d3ed3940339534428cf",
+    "url": "/static/media/Montserrat-BlackItalic.425e4899.eot"
+  },
+  {
+    "revision": "670916e5f28a8db7fadbdfc727325c10",
+    "url": "/static/media/Montserrat-BlackItalic.670916e5.woff"
+  },
+  {
+    "revision": "ab778b71ec25b374c3d13737bca464bb",
+    "url": "/static/media/Montserrat-BlackItalic.ab778b71.svg"
+  },
+  {
+    "revision": "ad31d82fca113d0dfdabb4c67bcabf14",
+    "url": "/static/media/Montserrat-BlackItalic.ad31d82f.ttf"
+  },
+  {
+    "revision": "f22318133e6e4342f7dba04de1ae0c56",
+    "url": "/static/media/Montserrat-BlackItalic.f2231813.woff2"
+  },
+  {
+    "revision": "f73aa932e8f29d07808e08cd844cbf6f",
+    "url": "/static/media/about6.f73aa932.jpg"
+  },
+  {
+    "revision": "07b50b4c540fdca6cce604daa120e252",
+    "url": "/static/media/bg-color.07b50b4c.jpg"
+  },
+  {
+    "revision": "c78ebe6ed4a8d137dfd051ef221e81a6",
+    "url": "/static/media/create-offer-decoration.c78ebe6e.png"
+  },
+  {
+    "revision": "8a98f8ee7ea62f9f5f0b5c217ef51466",
+    "url": "/static/media/create-offer.8a98f8ee.png"
+  },
+  {
+    "revision": "cfffd63d4e096270e44ea8e3ac19d947",
+    "url": "/static/media/home-bg-light.cfffd63d.webp"
+  },
+  {
+    "revision": "86c1faf90970691eb0f3253549c36b3c",
+    "url": "/static/media/home-bg.86c1faf9.jpg"
+  },
+  {
+    "revision": "6f5e424fedfef3a614caecd3fc9e6cea",
+    "url": "/static/media/join-decoraion.6f5e424f.png"
+  },
+  {
+    "revision": "354921336790f4bf7eb299e4826fa9a1",
+    "url": "/static/media/login-bg.35492133.jpg"
+  },
+  {
+    "revision": "36f87a857c32451363d3fd508833b1ed",
+    "url": "/static/media/payment.36f87a85.png"
+  },
+  {
+    "revision": "a4692d63a93f14a64f88747b6cf5a11f",
+    "url": "/static/media/sign-up-bg.a4692d63.jpg"
+  }
+]);
